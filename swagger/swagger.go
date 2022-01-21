72368d29d0af88ca6a40cfd2f5bf8eb8cd235373
Denis Palnitsky <denis@ukama.com>
2022-01-21 11:56:35 +0100

    Rename UI Handler func

--- swagger/swagger.go
+++ swagger/swagger.go
@@ -10,8 +10,8 @@ import (
 //go:embed swagger-ui
 var swaggerUiRes embed.FS
 
-// AddOpenApiUIHandler adds handler that serves html for Swagger UI
-func AddOpenApiUIHandler(ginEngine *gin.Engine, path string, openApiJsonPath string) {
+// AddUIHandler adds handler that serves html for Swagger UI
+func AddUIHandler(ginEngine *gin.Engine, path string, openApiJsonPath string) {
 	sub, err := fs.Sub(swaggerUiRes, "swagger-ui")
 	if err != nil {
 		panic(err)
